aa26ed9b59fe2469d38afef424c0cd2e8a95e38f
theallegrarr <nurudeen.amedu@gmail.com>
2019-07-08 23:24:15 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "node": true,
+        "browser": true
+    },
+    "rules":{
+        "./js/*.js": "off"
+    }
+};
