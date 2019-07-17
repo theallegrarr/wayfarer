c227687cef0d94bca1a6e8e44b3f95b08a9d5d0f
theallegrarr <nurudeen.amedu@gmail.com>
2019-07-17 16:55:00 +0100

    test modifications

--- js/tests/ausers.js
+++ js/tests/ausers.js
@@ -25,21 +25,6 @@ _chai2.default.use(_chaiHttp2.default);
 
 var should = _chai2.default.should();
 
-var data = {
-  bus_id: 1,
-  origin: 'Lagos',
-  destination: 'Kaduna',
-  trip_date: '2019-07-30T00:00:00.000Z',
-  fare: 8500,
-  status: 1
-};
-
-pool.query('INSERT INTO btrips(id, bus_id, origin, destination, trip_date, fare, status) VALUES($1, $2, $3, $4, $5, $6, $7)', [1, data.bus_id, data.origin, data.destination, data.trip_date, data.fare, data.status], function (err, res) {
-  if (err) {
-    console.log(err);
-  }
-});
-
 var details = {
   id: 9,
   email: 'kempo20@gmail.com',
--- src/tests/ausers.js
+++ src/tests/ausers.js
@@ -9,21 +9,6 @@ chai.use(chaiHttp);
 
 const should = chai.should();
 
-const data = {
-  bus_id: 1,
-  origin: 'Lagos',
-  destination: 'Kaduna',
-  trip_date: '2019-07-30T00:00:00.000Z',
-  fare: 8500,
-  status: 1,
-};
-
-pool.query('INSERT INTO btrips(id, bus_id, origin, destination, trip_date, fare, status) VALUES($1, $2, $3, $4, $5, $6, $7)', [1, data.bus_id, data.origin, data.destination, data.trip_date, data.fare, data.status], (err, res) => {
-  if (err) {
-    console.log(err);
-  }
-});
-
 const details = {
   id: 9,
   email: 'kempo20@gmail.com',
